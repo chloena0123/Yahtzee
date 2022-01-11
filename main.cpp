7e239b5d30d232056aa560d9ab809b573ad4f215
Quack <chloena0123@gmail.com>
2022-01-10 19:35:57 -0800

    init dice arr

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <bits/stdc++.h>
 #include "dice.h"
+#define NUM_DICE 5
 using namespace std;
 
 int main(){
-    Dice* d = new Dice();
-    cout << "h" << std::endl;
+    Dice** dice = new Dice*[NUM_DICE];
+    for(int i = 0; i < NUM_DICE; i++){
+        dice[i] = new Dice();
+    }
+    cout << dice[0]->getVal() << endl;
+
 }

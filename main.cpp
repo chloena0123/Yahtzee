aa6ce097cb125f3efc22b1b784b589758b3e21b9
Quack <chloena0123@gmail.com>
2022-01-10 19:37:15 -0800

    Revert "int main"
    This reverts commit a0867559

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
+#include <iostream>
 #include "dice.h"
-#define NUM_DICE 5
 using namespace std;
 
 int main(){
-    Dice** dice = new Dice*[NUM_DICE];
-    for(int i = 0; i < NUM_DICE; i++){
-        dice[i] = new Dice();
-    }
-    cout << dice[0]->getVal() << endl;
+    Dice* d = new Dice();
 
+    std::cout << d.getVal() << std::endl;
+
+    return 0;
 }

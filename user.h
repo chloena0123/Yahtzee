ed44913f2fee02d6224f7db8bfdec1441e2a7a58
Esther Cho <e35cho@uwaterloo.ca>
2022-01-20 23:49:43 -0500

    Class user variables

--- user.h
+++ user.h
@@ -4,14 +4,36 @@
 class User : Die{
 private:
     Die** dice;
+    int topTotal, bottomTotal, grandTotal;
+    int ones, twos, threes, fours, fives, sixes;
+    int threeOfaKind, fourOfaKind, fullHouse, smallStraight,largeStraight, yahtzee;
+
 public:
     User();
 
     void rollDice();
-
     void printDice();
-
-    bool checkYahtzee();
-
     void rollDice(int num);
+
+    int countOnes();
+    int countTwos();
+    int countThrees();
+    int countFours();
+    int countFives();
+    int countSixes();
+
+    int threeOfaKind();
+    int fourOfaKind();
+    int fullHouse();
+    int smallStraight();
+    int largeStraight();
+    int yahtzee();
+
+    void setTopTotal();
+    void setBottomTotal();
+    void setGrandTotal();
+    void getGrandTotal();
+
+
+
 };
